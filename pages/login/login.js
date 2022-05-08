d226bb924c153bcdcf7905f0b0f332cd75fa08d5
Rain <dcchen@stu.xidian.edu.cn>
2022-05-08 22:10:22 +0800

    完成test-login功能

--- pages/login/login.js
+++ pages/login/login.js
@@ -5,12 +5,53 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    account: "",
+    password: "",
+    result: -1
+  },
+
+  accountInput(e) {
+    this.setData({
+      account: e.detail.value
+    })
+  },
+
+  pwdInput(e) {
+    this.setData({
+      password: e.detail.value
+    })
   },
 
   /**
    * form 表单 保存、重置操作
    */
+  loginTest() {
+    console.log("test-login!")
+    console.log(this.data.account)
+    console.log(this.data.password)
+    var that = this
+
+    wx.request({
+      //请求接口的地址
+      url: "http://dcac.top:5000/test-login",
+      data: {
+        username: this.data.account,
+        password: this.data.password,
+      },
+      header: {
+        'Content-Type' : 'application/x-www-form-urlencoded'
+      },
+      method: "POST",
+      success: function (res) {
+        console.log(res.data);
+        that.setData({code: res.data.code})
+        // term_begin_day = new Date(res.data["data"])
+      },
+      fail: function (err) {console.log(err);}, //请求失败
+      complete: function () {} //请求完成后执行的函数
+    })
+  },
+
   formSubmit(e) {
     console.log('form发生了submit事件，携带数据为：', e.detail.value)
   },
